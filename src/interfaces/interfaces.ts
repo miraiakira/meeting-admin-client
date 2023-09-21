b30c4790442c971c2e40c0ffe54c605fde4c9c26
sailbirds <sailbirds@luminesence.jp>
2023-09-22 03:04:26 +0900

    feat: admin user udpate

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,85 +1,124 @@
-import { message } from "antd";
-import axios from "axios";
+import { message } from 'antd';
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:5000/",
-	timeout: 3000,
+  baseURL: 'http://localhost:5500/',
+  timeout: 3000,
 });
 
 axiosInstance.interceptors.request.use(function (config) {
-	const accessToken = localStorage.getItem("access_token");
+  const accessToken = localStorage.getItem('access_token');
 
-	if (accessToken) {
-		config.headers.authorization = "Bearer " + accessToken;
-	}
-	return config;
+  if (accessToken) {
+    config.headers.authorization = 'Bearer ' + accessToken;
+  }
+  return config;
 });
 
 axiosInstance.interceptors.response.use(
-	(response) => {
-		return response;
-	},
-	async (error) => {
-		let { data, config } = error.response;
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    let { data, config } = error.response;
 
-		if (data.code === 401 && !config.url.includes("/user/admin/refresh")) {
-			const res = await refreshToken();
+    if (data.code === 401 && !config.url.includes('/user/admin/refresh')) {
+      const res = await refreshToken();
 
-			if (res.status === 200) {
-				return axios(config);
-			} else {
-				message.error(res.data);
-
-				setTimeout(() => {
-					window.location.href = "/login";
-				}, 1500);
-			}
-		} else {
-			return error.response;
-		}
-	}
+      if (res.status === 200) {
+        return axiosInstance(config);
+      } else {
+        message.error(res.data);
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 1500);
+      }
+    } else {
+      return error.response;
+    }
+  }
 );
 
 async function refreshToken() {
-	const res = await axiosInstance.get("/user/admin/refresh", {
-		params: {
-			refresh_token: localStorage.getItem("refresh_token"),
-		},
-	});
-	localStorage.setItem("access_token", res.data.access_token);
-	localStorage.setItem("refresh_token", res.data.refresh_token);
-	return res;
+  const res = await axiosInstance.get('/user/admin/refresh', {
+    params: {
+      refreshToken: localStorage.getItem('refresh_token'),
+    },
+  });
+  localStorage.setItem('access_token', res.data.data.access_token);
+  localStorage.setItem('refresh_token', res.data.data.refresh_token);
+  return res;
 }
 
 export async function login(username: string, password: string) {
-	return await axiosInstance.post("/user/admin/login", {
-		username,
-		password,
-	});
+  return await axiosInstance.post('/user/admin/login', {
+    username,
+    password,
+  });
 }
 
 export async function userSearch(
-	username: string,
-	nickName: string,
-	email: string,
-	pageNo: number,
-	pageSize: number
+  username: string,
+  nickName: string,
+  email: string,
+  pageNo: number,
+  pageSize: number
 ) {
-	return await axiosInstance.get("/user/list", {
-		params: {
-			username,
-			nickName,
-			email,
-			pageNo,
-			pageSize,
-		},
-	});
+  return await axiosInstance.get('/user/list', {
+    params: {
+      username,
+      nickName,
+      email,
+      pageNo,
+      pageSize,
+    },
+  });
 }
 
 export async function freeze(id: number) {
-	return await axiosInstance.get("/user/freeze", {
-		params: {
-			id,
-		},
-	});
+  return await axiosInstance.get('/user/freeze', {
+    params: {
+      id,
+    },
+  });
+}
+
+export async function getUserInfo() {
+  return await axiosInstance.get('/user/info');
+}
+
+export async function updateInfo(data: UserInfo) {
+  return await axiosInstance.post('/user/admin/update', data);
+}
+
+export async function updateUserInfoCaptcha() {
+  return await axiosInstance.get('/user/update/captcha');
+}
+
+export async function updatePasswordCaptcha(email: string) {
+  return await axiosInstance.get('/user/update_password/captcha', {
+    params: {
+      address: email,
+    },
+  });
+}
+
+export async function updatePassword(data: UpdatePassword) {
+  return await axiosInstance.post('/user/admin/update_password', data);
+}
+
+export interface UserInfo {
+  username: string;
+  headPic: string;
+  nickName: string;
+  email: string;
+  captcha: string;
+}
+
+export interface UpdatePassword {
+  email: string;
+  captcha: string;
+  password: string;
+  confirmPassword: string;
+  username?: string;
 }
