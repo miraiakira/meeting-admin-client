ab5ceb08ed7b23374f04937716f7f0ec346315b2
xdong <xiaoming.dong@omcon.co.jp>
2024-11-28 11:32:04 +0900

    chore: update user update & password update

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -3,269 +3,273 @@ import axios from "axios";
 import dayjs from "dayjs";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:5000/",
-	timeout: 3000,
+  baseURL: "http://localhost:3700/",
+  timeout: 3000,
 });
 
 axiosInstance.interceptors.request.use(function (config) {
-	const accessToken = localStorage.getItem("access_token");
+  const accessToken = localStorage.getItem("access_token");
 
-	if (accessToken) {
-		config.headers.authorization = "Bearer " + accessToken;
-	}
-	return config;
+  if (accessToken) {
+    config.headers.authorization = "Bearer " + accessToken;
+  }
+  return config;
 });
 
 axiosInstance.interceptors.response.use(
-	(response) => {
-		return response;
-	},
-	async (error) => {
-		let { data, config } = error.response;
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    let { data, config } = error.response;
 
-		if (data.code === 401 && !config.url.includes("/user/admin/refresh")) {
-			const res = await refreshToken();
+    if (data.code === 401 && !config.url.includes("/user/admin/refresh")) {
+      const res = await refreshToken();
 
-			if (res.status === 200) {
-				return axiosInstance(config);
-			} else {
-				message.error(res.data);
-				setTimeout(() => {
-					window.location.href = "/login";
-				}, 1500);
-			}
-		} else {
-			return error.response;
-		}
-	}
+      if (res.status === 200) {
+        return axiosInstance(config);
+      } else {
+        message.error(res.data);
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1500);
+      }
+    } else {
+      return error.response;
+    }
+  }
 );
 
 async function refreshToken() {
-	const res = await axiosInstance.get("/user/admin/refresh", {
-		params: {
-			refreshToken: localStorage.getItem("refresh_token"),
-		},
-	});
-	localStorage.setItem("access_token", res.data.data.access_token);
-	localStorage.setItem("refresh_token", res.data.data.refresh_token);
-	return res;
+  const res = await axiosInstance.get("/user/admin/refresh", {
+    params: {
+      refreshToken: localStorage.getItem("refresh_token"),
+    },
+  });
+  localStorage.setItem("access_token", res.data.data.access_token);
+  localStorage.setItem("refresh_token", res.data.data.refresh_token);
+  return res;
 }
 
 export async function login(username: string, password: string) {
-	return await axiosInstance.post("/user/admin/login", {
-		username,
-		password,
-	});
+  return await axiosInstance.post("/user/admin/login", {
+    username,
+    password,
+  });
 }
 
 export async function userSearch(
-	username: string,
-	nickName: string,
-	email: string,
-	pageNo: number,
-	pageSize: number
+  username: string,
+  nickName: string,
+  email: string,
+  pageNo: number,
+  pageSize: number
 ) {
-	return await axiosInstance.get("/user/list", {
-		params: {
-			username,
-			nickName,
-			email,
-			pageNo,
-			pageSize,
-		},
-	});
+  return await axiosInstance.get("/user/list", {
+    params: {
+      username,
+      nickName,
+      email,
+      pageNo,
+      pageSize,
+    },
+  });
 }
 
 export async function freeze(id: number) {
-	return await axiosInstance.get("/user/freeze", {
-		params: {
-			id,
-		},
-	});
+  return await axiosInstance.get("/user/freeze", {
+    params: {
+      id,
+    },
+  });
 }
 
 export async function getUserInfo() {
-	return await axiosInstance.get("/user/info");
+  return await axiosInstance.get("/user/info");
 }
 
 export async function updateInfo(data: UserInfo) {
-	return await axiosInstance.post("/user/admin/update", data);
+  return await axiosInstance.post("/user/admin/update", data);
 }
 
 export async function updateUserInfoCaptcha() {
-	return await axiosInstance.get("/user/update/captcha");
+  return await axiosInstance.get("/user/update/captcha");
 }
 
 export async function updatePasswordCaptcha(email: string) {
-	return await axiosInstance.get("/user/update_password/captcha", {
-		params: {
-			address: email,
-		},
-	});
+  return await axiosInstance.get("/user/update_password/captcha", {
+    params: {
+      address: email,
+    },
+  });
 }
 
 export async function updatePassword(data: UpdatePassword) {
-	return await axiosInstance.post("/user/admin/update_password", data);
+  return await axiosInstance.post("/user/admin/update_password", data);
 }
 
 export async function meetingRoomList(
-	name: string,
-	capacity: number,
-	equipment: string,
-	pageNo: number,
-	pageSize: number
+  name: string,
+  capacity: number,
+  equipment: string,
+  pageNo: number,
+  pageSize: number
 ) {
-	return await axiosInstance.get("/meeting-room/list", {
-		params: {
-			name,
-			capacity,
-			equipment,
-			pageNo,
-			pageSize,
-		},
-	});
+  return await axiosInstance.get("/meeting-room/list", {
+    params: {
+      name,
+      capacity,
+      equipment,
+      pageNo,
+      pageSize,
+    },
+  });
 }
 
 export async function deleteMeetingRoom(id: number) {
-	return await axiosInstance.delete("/meeting-room/" + id);
+  return await axiosInstance.delete("/meeting-room/" + id);
 }
 
 export async function createMeetingRoom(meetingRoom: CreateMeetingRoom) {
-	return await axiosInstance.post("/meeting-room/create", meetingRoom);
+  return await axiosInstance.post("/meeting-room/create", meetingRoom);
 }
 
 export async function updateMeetingRoom(meetingRoom: UpdateMeetingRoom) {
-	return await axiosInstance.put("/meeting-room/update", meetingRoom);
+  return await axiosInstance.put("/meeting-room/update", meetingRoom);
 }
 
 export async function findMeetingRoom(id: number) {
-	return await axiosInstance.get("/meeting-room/" + id);
+  return await axiosInstance.get("/meeting-room/" + id);
 }
 
 export interface UserInfo {
-	username: string;
-	headPic: string;
-	nickName: string;
-	email: string;
-	captcha: string;
+  username: string;
+  headPic: string;
+  nickName: string;
+  email: string;
+  captcha: string;
 }
 
 export interface UpdatePassword {
-	email: string;
-	captcha: string;
-	password: string;
-	confirmPassword: string;
-	username?: string;
+  email: string;
+  captcha: string;
+  password: string;
+  confirmPassword: string;
+  username?: string;
 }
 
 export interface CreateMeetingRoom {
-	name: string;
-	capacity: number;
-	location: string;
-	equipment: string;
-	description: string;
+  name: string;
+  capacity: number;
+  location: string;
+  equipment: string;
+  description: string;
 }
 
 export interface UpdateMeetingRoom {
-	id: number;
-	name: string;
-	capacity: number;
-	location: string;
-	equipment: string;
-	description: string;
+  id: number;
+  name: string;
+  capacity: number;
+  location: string;
+  equipment: string;
+  description: string;
 }
 
 export interface SearchBooking {
-	username: string;
-	meetingRoomName: string;
-	meetingRoomPosition: string;
-	rangeStartDate: Date;
-	rangeStartTime: Date;
-	rangeEndDate: Date;
-	rangeEndTime: Date;
+  username: string;
+  meetingRoomName: string;
+  meetingRoomPosition: string;
+  rangeStartDate: Date;
+  rangeStartTime: Date;
+  rangeEndDate: Date;
+  rangeEndTime: Date;
 }
 
 export async function bookingList(
-	searchBooking: SearchBooking,
-	pageNo: number,
-	pageSize: number
+  searchBooking: SearchBooking,
+  pageNo: number,
+  pageSize: number
 ) {
-	let bookingTimeRangeStart;
-	let bookingTimeRangeEnd;
+  let bookingTimeRangeStart;
+  let bookingTimeRangeEnd;
 
-	if (searchBooking.rangeStartDate && searchBooking.rangeStartTime) {
-		const rangeStartDateStr = dayjs(searchBooking.rangeStartDate).format(
-			"YYYY-MM-DD"
-		);
-		const rangeStartTimeStr = dayjs(searchBooking.rangeStartTime).format(
-			"HH:mm"
-		);
-		bookingTimeRangeStart = dayjs(
-			rangeStartDateStr + " " + rangeStartTimeStr
-		).valueOf();
-	}
+  if (searchBooking.rangeStartDate && searchBooking.rangeStartTime) {
+    const rangeStartDateStr = dayjs(searchBooking.rangeStartDate).format(
+      "YYYY-MM-DD"
+    );
+    const rangeStartTimeStr = dayjs(searchBooking.rangeStartTime).format(
+      "HH:mm"
+    );
+    bookingTimeRangeStart = dayjs(
+      rangeStartDateStr + " " + rangeStartTimeStr
+    ).valueOf();
+  }
 
-	if (searchBooking.rangeEndDate && searchBooking.rangeEndTime) {
-		const rangeEndDateStr = dayjs(searchBooking.rangeEndDate).format(
-			"YYYY-MM-DD"
-		);
-		const rangeEndTimeStr = dayjs(searchBooking.rangeEndTime).format("HH:mm");
-		bookingTimeRangeEnd = dayjs(
-			rangeEndDateStr + " " + rangeEndTimeStr
-		).valueOf();
-	}
+  if (searchBooking.rangeEndDate && searchBooking.rangeEndTime) {
+    const rangeEndDateStr = dayjs(searchBooking.rangeEndDate).format(
+      "YYYY-MM-DD"
+    );
+    const rangeEndTimeStr = dayjs(searchBooking.rangeEndTime).format("HH:mm");
+    bookingTimeRangeEnd = dayjs(
+      rangeEndDateStr + " " + rangeEndTimeStr
+    ).valueOf();
+  }
 
-	return await axiosInstance.get("/booking/list", {
-		params: {
-			username: searchBooking.username,
-			meetingRoomName: searchBooking.meetingRoomName,
-			meetingRoomPosition: searchBooking.meetingRoomPosition,
-			bookingTimeRangeStart,
-			bookingTimeRangeEnd,
-			pageNo: pageNo,
-			pageSize: pageSize,
-		},
-	});
+  return await axiosInstance.get("/booking/list", {
+    params: {
+      username: searchBooking.username,
+      meetingRoomName: searchBooking.meetingRoomName,
+      meetingRoomPosition: searchBooking.meetingRoomPosition,
+      bookingTimeRangeStart,
+      bookingTimeRangeEnd,
+      pageNo: pageNo,
+      pageSize: pageSize,
+    },
+  });
 }
 
 export async function apply(id: number) {
-	return await axiosInstance.get("/booking/apply/" + id);
+  return await axiosInstance.get("/booking/apply/" + id);
 }
 
 export async function reject(id: number) {
-	return await axiosInstance.get("/booking/reject/" + id);
+  return await axiosInstance.get("/booking/reject/" + id);
 }
 
 export async function unbind(id: number) {
-	return await axiosInstance.get("/booking/unbind/" + id);
+  return await axiosInstance.get("/booking/unbind/" + id);
 }
 
 export async function meetingRoomUsedCount(startTime: string, endTime: string) {
-	return await axiosInstance.get("/statistic/meetingRoomUsedCount", {
-		params: {
-			startTime,
-			endTime,
-		},
-	});
+  return await axiosInstance.get("/statistic/meetingRoomUsedCount", {
+    params: {
+      startTime,
+      endTime,
+    },
+  });
 }
 
 export async function userBookingCount(startTime: string, endTime: string) {
-	return await axiosInstance.get("/statistic/userBookingCount", {
-		params: {
-			startTime,
-			endTime,
-		},
-	});
+  return await axiosInstance.get("/statistic/userBookingCount", {
+    params: {
+      startTime,
+      endTime,
+    },
+  });
 }
 
 export interface UserBookingData {
-	userId: string;
-	username: string;
-	bookingCount: string;
+  userId: string;
+  username: string;
+  bookingCount: string;
 }
 
 export interface MeetingRoomUsedData {
-	meetingRoomName: string;
-	meetingRoomId: number;
-	usedCount: string;
+  meetingRoomName: string;
+  meetingRoomId: number;
+  usedCount: string;
+}
+
+export async function presignedUrl(fileName: string) {
+  return axiosInstance.get(`/minio/presignedUrl?name=${fileName}`);
 }
--- src/pages/InfoModify/InfoModify.tsx
+++ src/pages/InfoModify/InfoModify.tsx
@@ -1,15 +1,15 @@
-import { Button, Form, Input, message, Space } from 'antd';
-import { useForm } from 'antd/es/form/Form';
-import { useCallback, useEffect, useState } from 'react';
-import './info_modify.css';
-import { useNavigate } from 'react-router-dom';
-import { HeadPicUpload } from './HeadPicUpload';
+import { Button, Form, Input, message, Space } from "antd";
+import { useForm } from "antd/es/form/Form";
+import { useCallback, useEffect, useState } from "react";
+import "./info_modify.css";
+import { useNavigate } from "react-router-dom";
+import { HeadPicUpload } from "./HeadPicUpload";
 import {
   getUserInfo,
   updateInfo,
   updateUserInfoCaptcha,
   UserInfo,
-} from '../../interfaces/interfaces';
+} from "../../interfaces/interfaces";
 
 const layout1 = {
   labelCol: { span: 6 },
@@ -25,13 +25,13 @@ export function InfoModify() {
 
     if (res.status === 201 || res.status === 200) {
       const { message: msg, data } = res.data;
-      if (msg === 'success') {
-        message.success('用户信息更新成功');
+      if (msg === "success") {
+        message.success("用户信息更新成功");
       } else {
         message.error(data);
       }
     } else {
-      message.error('系统繁忙，请稍后再试');
+      message.error(res?.data?.data || "系统繁忙，请稍后再试");
     }
   }, []);
 
@@ -40,7 +40,7 @@ export function InfoModify() {
     if (res.status === 201 || res.status === 200) {
       message.success(res.data.data);
     } else {
-      message.error('系统繁忙，请稍后再试');
+      message.error("系统繁忙，请稍后再试");
     }
   }, []);
 
@@ -51,9 +51,9 @@ export function InfoModify() {
       const { data } = res.data;
 
       if (res.status === 201 || res.status === 200) {
-        form.setFieldValue('headPic', data.headPic);
-        form.setFieldValue('nickName', data.nickName);
-        form.setFieldValue('email', data.email);
+        form.setFieldValue("headPic", data.headPic);
+        form.setFieldValue("nickName", data.nickName);
+        form.setFieldValue("email", data.email);
       }
     }
     query();
@@ -71,7 +71,7 @@ export function InfoModify() {
         <Form.Item
           label="头像"
           name="headPic"
-          rules={[{ required: true, message: '请输入头像!' }]}
+          rules={[{ required: true, message: "请输入头像!" }]}
           shouldUpdate
         >
           <HeadPicUpload></HeadPicUpload>
@@ -80,7 +80,7 @@ export function InfoModify() {
         <Form.Item
           label="昵称"
           name="nickName"
-          rules={[{ required: true, message: '请输入昵称!' }]}
+          rules={[{ required: true, message: "请输入昵称!" }]}
         >
           <Input />
         </Form.Item>
@@ -89,8 +89,8 @@ export function InfoModify() {
           label="邮箱"
           name="email"
           rules={[
-            { required: true, message: '请输入邮箱!' },
-            { type: 'email', message: '请输入合法邮箱地址!' },
+            { required: true, message: "请输入邮箱!" },
+            { type: "email", message: "请输入合法邮箱地址!" },
           ]}
         >
           <Input disabled />
@@ -99,9 +99,9 @@ export function InfoModify() {
         <Form.Item
           label="验证码"
           name="captcha"
-          rules={[{ required: true, message: '请输入验证码!' }]}
+          rules={[{ required: true, message: "请输入验证码!" }]}
         >
-          <Space.Compact style={{ display: 'flex' }}>
+          <Space.Compact style={{ display: "flex" }}>
             <Input />
             <Button type="primary" onClick={sendCaptcha}>
               发送验证码
